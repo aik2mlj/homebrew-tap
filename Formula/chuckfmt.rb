f58a9ada152abf63fa1696f0ae875ab32175ed57
Lejun Min <code@lejun.site>
2026-04-14 15:14:50 -0700

    chuckfmt v0.2.1

--- Formula/chuckfmt.rb
+++ Formula/chuckfmt.rb
@@ -2,20 +2,20 @@ class Chuckfmt < Formula
   desc "A fast code formatter for ChucK programming language"
   homepage "https://github.com/aik2mlj/chuckfmt"
   license "GPL-3.0-or-later"
-  version "0.2.0"
+  version "0.2.1"
 
   on_macos do
     if Hardware::CPU.intel?
       url "#{homepage}/releases/download/v#{version}/chuckfmt-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "ab1e1137edf7b85db20de2212ca41c5eafd6f04ccfb43ca28c3834336f500523"
+      sha256 "3632e433475fd7443536e5e85f457e4fd659fd989d4e0f86efbb4f509fb61aeb"
     else
       url "#{homepage}/releases/download/v#{version}/chuckfmt-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "10367f0b54bcc201542abd0269031a31d4acb4836e0fd2431c118bc7cd9d3523"
+      sha256 "9fbd680644a970feafb4fce0ebaef3835ce2442375bfb9acc1b72961ac1c0022"
     end
   end
   on_linux do
     url "#{homepage}/releases/download/v#{version}/chuckfmt-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "fad1a2c9e7f21af2768335e996ef7aec28f90de8677355b5fd2e5f207fb6a7e8"
+    sha256 "fd9107d8c9b3f85c9dd04c7f53fafa27f6c14567effaf51fbbe820d73529c601"
   end
 
   def install
